c67bd25d42dee0bedbf8bcc2f6d87aff029c9e1f
soyKabukiDev <mrlatec2001@gmail.com>
2025-05-23 22:15:49 -0600

    Versión 1 del proyecto

---
+++ contact.php
@@ -0,0 +1,262 @@
+﻿<!DOCTYPE HTML>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Contacto - Rair Virtual</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="Html5TemplatesDreamweaver.com">
+
+  <link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+  <!-- Icons -->
+  <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet"
+    type="text/css" />
+  <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+  <!--[if lt IE 8]>
+        <link href="scripts/icons/general/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+        <link href="scripts/icons/social/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+    <![endif]-->
+  <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
+  <!--[if IE 7]>
+        <link rel="stylesheet" href="scripts/fontawesome/css/font-awesome-ie7.min.css">
+    <![endif]-->
+
+  <link href="scripts/wookmark/css/style.css" rel="stylesheet" type="text/css" />
+  <link href="scripts/yoxview/yoxview.css" rel="stylesheet" type="text/css" />
+  <link href="http://fonts.googleapis.com/css?family=Syncopate" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
+  <link href="http://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet" type="text/css">
+
+  <link href="styles/custom.css" rel="stylesheet" type="text/css" />
+  <script src="email/validation.js" type="text/javascript"></script>
+</head>
+
+<body id="pageBody">
+
+  <div id="divBoxed" class="container">
+
+    <div class="transparent-bg" style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: -1;zoom: 1;">
+    </div>
+
+    <div class="divPanel notop nobottom">
+      <div class="row-fluid">
+        <div class="span12">
+          <div id="divLogo" class="pull-left">
+            <a href="index.html" id="divSiteTitle">
+              <img src="images/logo.png" alt="Universidad de Yahualica" style="width: 20rem" />
+            </a>
+          </div>
+
+          <div id="divMenuRight" class="pull-right">
+            <div class="navbar">
+              <button type="button" class="btn btn-navbar-highlight btn-large btn-primary" data-toggle="collapse"
+                data-target=".nav-collapse">
+                NAVIGATION <span class="icon-chevron-down icon-white"></span>
+              </button>
+              <div class="nav-collapse collapse">
+                <ul class="nav nav-pills ddmenu">
+                  <li><a href="index.html">Inicio</a></li>
+                  <li><a href="about.html">Acerca de</a></li>
+                  <li><a href="publish-process.html">Proceso de Publicación</a></li>
+                  <li><a href="publications.html">Publicaciones</a></li>
+                  <li class="active"><a href="contact.php">Contacto</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row-fluid">
+        <div class="span12">
+          <div id="headerSeparator"></div>
+
+          <div class="camera_full_width">
+            <div id="camera_wrap">
+              <div data-src="slider-images/4.jpg">
+                <!--<div class="camera_caption fadeFromBottom cap2">Add a slogan or feature here...</div> -->
+              </div>
+              <div data-src="slider-images/1.jpg"></div>
+              <div data-src="slider-images/2.jpg"></div>
+            </div>
+            <br style="clear: both" />
+            <div style="margin-bottom: 40px"></div>
+          </div>
+
+          <div id="headerSeparator2"></div>
+        </div>
+      </div>
+    </div>
+
+    <div class="contentArea">
+
+      <div class="divPanel notop page-content">
+
+        <div class="breadcrumbs">
+          <a href="index.html">Home</a> &nbsp;/&nbsp; <span>Contact</span>
+        </div>
+
+        <div class="row-fluid">
+          <div class="span8" id="divMain">
+
+            <h1>Contáctanos</h1>
+            <h3 style="color:#FF6633;"><?php echo @$_GET['msg']; ?></h3>
+            <hr>
+            <!--Start Contact form -->
+            <form name="enq" method="post" action="email/" onsubmit="return validation();">
+              <fieldset>
+
+                <input type="text" name="name" id="name" value="" class="input-block-level" placeholder="Name" />
+                <input type="text" name="email" id="email" value="" class="input-block-level" placeholder="Email" />
+                <textarea rows="11" name="message" id="message" class="input-block-level"
+                  placeholder="Comments"></textarea>
+                <div class="actions">
+                  <input type="submit" value="Send Your Message" name="submit" id="submitButton"
+                    class="btn btn-info pull-right" title="Click here to submit your message!" />
+                </div>
+
+              </fieldset>
+            </form>
+            <!--End Contact form -->
+          </div>
+
+          <!--Edit Sidebar Content here-->
+          <div class="span4 sidebar">
+
+            <div class="sidebox">
+              <h3 class="sidebox-title">Información de Contacto:</h3>
+              <p>
+              <address><strong>Your Company, Inc.</strong><br />
+                Address<br />
+                City, State, Zip<br />
+                <abbr title="Phone">P:</abbr> (123) 456-7890
+              </address>
+              <address> <strong>Email</strong><br />
+                <a href="mailto:#">first.last@gmail.com</a>
+              </address>
+              </p>
+
+              <p>
+                <strong>Social Media</strong><br />
+                <a href="http://www.facebook.com/" target="_blank" title="Facebook"><i
+                    class="icon-facebook-sign icon-2x"></i></a>
+                <a href="http://www.twitter.com/" target="_blank" title="Twitter"><i
+                    class="icon-twitter-sign icon-2x"></i></a>
+                <a href="http://www.linkedin.com/" target="_blank" title="LinkedIn"><i
+                    class="icon-linkedin-sign icon-2x"></i></a>
+
+            </div>
+          </div>
+          <!--/End Sidebar Content-->
+
+
+        </div>
+
+        <div id="footerInnerSeparator"></div>
+      </div>
+    </div>
+
+    <div id="footerOuterSeparator"></div>
+
+    <div id="divFooter" class="footerArea">
+      <div class="divPanel">
+        <div class="row-fluid">
+          <div class="span6" id="footerArea1">
+            <h3>Nosotros</h3>
+
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry’s standard dummy
+              text ever since the 1500s.
+            </p>
+          </div>
+          <div class="span3" id="footerArea2">
+            <h3>Siguenos en</h3>
+            <p style="display: flex; align-items: center; gap: 8px; margin: 0;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="auto" fill="currentColor"
+                viewBox="0 0 24 24" aria-hidden="true">
+                <path fill-rule="evenodd"
+                  d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+                  clip-rule="evenodd" />
+              </svg>
+              <a href="#" title="Facebook"
+                style="text-decoration: none; color: #5e82b1; font-weight: 600; font-size: .80rem;">
+                Facebook
+              </a>
+            </p>
+
+          </div>
+          <div class="span3" id="footerArea3">
+            <h3>Contactanos</h3>
+            <p style="display: flex; align-items: center; gap: 8px; margin: 0;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="auto" fill="currentColor"
+                viewBox="0 0 24 24" aria-hidden="true">
+                <path fill-rule="evenodd"
+                  d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z"
+                  clip-rule="evenodd" />
+                <path
+                  d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"
+                  fill="currentColor" />
+              </svg>
+              <a href="#" title="Whatsapp"
+                style="text-decoration: none; color: #5e82b1; font-weight: 600; font-size: .80rem;">
+                Whatsapp
+              </a>
+            </p>
+
+            <p style="display: flex; align-items: center; gap: 8px; margin: 0;">
+              <svg xmlns="http://www.w3.org/2000/svg" width="1.7rem" height="1.1rem" fill="currentColor"
+                viewBox="0 0 24 18" aria-hidden="true">
+                <path
+                  d="M2 2a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v0.5L12 9 2 2.5V2zm0 2.8V16a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4.8l-10 6.7L2 4.8z" />
+              </svg>
+              <a href="mailto:ejemplo@correo.com" title="Email"
+                style="text-decoration: none; color: #5e82b1; font-weight: 600; font-size: .80rem;">
+                Email
+              </a>
+            </p>
+
+
+          </div>
+        </div>
+
+        <br /><br />
+        <div class="row-fluid">
+          <div class="span12">
+            <p class="copyright">
+              Copyright © 2013 Your Company. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br /><br /><br />
+
+  <script src="scripts/jquery.min.js" type="text/javascript"></script>
+  <script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="scripts/default.js" type="text/javascript"></script>
+
+  <script src="scripts/wookmark/js/jquery.wookmark.js" type="text/javascript"></script>
+  <script
+    type="text/javascript">$(window).load(function () { var options = { autoResize: true, container: $('#gridArea'), offset: 10 }; var handler = $('#tiles li'); handler.wookmark(options); $('#tiles li').each(function () { var imgm = 0; if ($(this).find('img').length > 0) imgm = parseInt($(this).find('img').not('p img').css('margin-bottom')); var newHeight = $(this).find('img').height() + imgm + $(this).find('div').height() + $(this).find('h4').height() + $(this).find('p').not('blockquote p').height() + $(this).find('iframe').height() + $(this).find('blockquote').height() + 5; if ($(this).find('iframe').height()) newHeight = newHeight + 15; $(this).css('height', newHeight + 'px'); }); handler.wookmark(options); handler.wookmark(options); });</script>
+  <script src="scripts/yoxview/yox.js" type="text/javascript"></script>
+  <script src="scripts/yoxview/jquery.yoxview-2.21.js" type="text/javascript"></script>
+  <script
+    type="text/javascript">$(document).ready(function () { $('.yoxview').yoxview({ autoHideInfo: false, renderInfoPin: false, backgroundColor: '#ffffff', backgroundOpacity: 0.8, infoBackColor: '#000000', infoBackOpacity: 1 }); $('.yoxview a img').hover(function () { $(this).animate({ opacity: 0.7 }, 300) }, function () { $(this).animate({ opacity: 1 }, 300) }); });</script>
+
+
+</body>
+
+</html>
